0f677de943c836541770c4a3cd0265ff92090de5
Serious03Black <prathaamesh2509@gmail.com>
2024-08-27 16:48:58 +0530

    day 27-08-24 datatype confusion chai aur code

---
+++ dataType_ECME.js
@@ -0,0 +1,20 @@
+"use Strict"
+// treat all code as new version
+// console.log("hello wrold")
+
+//data type 
+// nummber => range  2**53 
+// bigint for more than int
+// string => for charcter ""
+// boolean => true/false
+// null => standlone for empty 
+// undefined=value not assign
+// symbole => unique
+
+// object 
+console.log(typeof"Prathmesh");
+// string
+console.log(typeof null);
+// object
+console.log(typeof undefined);
+// undefined
