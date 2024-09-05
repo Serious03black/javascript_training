7639a5b657646490b95329e7f83a03727d737f43
Serious03Black <prathaamesh2509@gmail.com>
2024-09-05 18:42:30 +0530

    lec9 datatype summary

---
+++ lec9_DataTypeofJsSummary.js
@@ -0,0 +1,28 @@
+//Primirive 
+//7 types : String,Number,Boolean,null,undefined,symbole,BigInt
+// const score=100;
+// const scoreVal=100.3
+// const islogedIn=false
+// const outsideTemp=null
+let userEmail;
+
+const id =Symbol('123')
+const anotherid=Symbol('123')
+console.log(id === anotherid);//false
+const bigNumber=121212322n;//bigint by adding n
+
+//refernce type (Non primitive)
+// Array ,objects,functions 
+
+const heros=["shaktiman","naagraj","doga"] //array
+
+let myobj={//object
+    name:"prathmesh",
+    age:22,
+}
+
+const varr = function(){ //function
+console.log("radhe radhe")
+}
+
+// all DataType of all non premitive is object
